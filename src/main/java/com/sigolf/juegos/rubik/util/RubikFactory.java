f4778d5684411728de3db7505415e66df486826e
narisant <narisant@BOG-1ZJGZF2.usersad.everis.int>
2017-11-11 16:11:42 -0500

    adelanto en rubik 5x5

--- src/main/java/com/sigolf/juegos/rubik/util/RubikFactory.java
+++ src/main/java/com/sigolf/juegos/rubik/util/RubikFactory.java
@@ -3,6 +3,7 @@ package com.sigolf.juegos.rubik.util;
 import com.sigolf.juegos.rubik.modelo.CuboRubik2x2;
 import com.sigolf.juegos.rubik.modelo.CuboRubik3x3;
 import com.sigolf.juegos.rubik.modelo.CuboRubik4x4;
+import com.sigolf.juegos.rubik.modelo.CuboRubik5x5;
 import com.sigolf.juegos.rubik.modelo.Puzzle;
 import com.sigolf.juegos.rubik.modelo.Skewb;
 
@@ -16,6 +17,8 @@ public class RubikFactory {
 			return new CuboRubik3x3(tipoCubo, "3X3X3");
 		case 7:
 			return new CuboRubik4x4(tipoCubo, "4X4X4");
+		case 8:
+			return new CuboRubik5x5(tipoCubo, "5X5X5");
 		case 11:
 			return new CuboRubik3x3(tipoCubo, "3X3X3 BLD");
 		case 12:
@@ -24,6 +27,8 @@ public class RubikFactory {
 			return new CuboRubik3x3(tipoCubo, "3X3X3 OH");
 		case 14:
 			return new CuboRubik4x4(tipoCubo, "4X4X4 BLD");
+		case 15:
+			return new CuboRubik5x5(tipoCubo, "5X5X5 BLD");
 		case 24:
 			return new Skewb(tipoCubo, "Skewb");
 		default:
