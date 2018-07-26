eec0fc2497f642816cdfcfa49ecd8f509fb77b87
Nelson Ariza <njarizas@hotmail.com>
2018-07-26 13:52:35 -0500

    correccion pequeña

--- src/main/java/xyz/njas/controlador/managedbeans/session/RecuperarUsuarioManagedBean.java
+++ src/main/java/xyz/njas/controlador/managedbeans/session/RecuperarUsuarioManagedBean.java
@@ -93,10 +93,10 @@ public class RecuperarUsuarioManagedBean {
 
 	public String generarClaveAleatoria() {
 		char[] caracteres;
-		caracteres = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '#', '$', '%', '&', '+', '-', '*', '~', '^', '<', '>', '@', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+		caracteres = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '#', '$', '%', '+', '-', '*', '~', '^', '@', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 		String pass = "";
 		for (int i = 0; i < 12; i++) {
-			pass += caracteres[new Random().nextInt(75)];
+			pass += caracteres[new Random().nextInt(caracteres.length+1)];
 		}
 		return pass;
 	}
